84491d5b3edbb42e6311ccfe412e5f8f2b75aa3f
Aellon Krider <aellonkrider@gmail.com>
2017-01-28 16:45:17 -0500

    update nicotine.js?

--- nicotine.js
+++ nicotine.js
@@ -28,11 +28,6 @@ $(document).on('click', 'a[href^="#"]', function(e) {
     // prevent standard hash navigation (avoid blinking in IE)
     e.preventDefault();
 
-    // top position relative to the document
-    var pos = $(id).offset().top;
-
-    // animated top scrolling
-    $('body, html').animate({scrollTop: pos});
 });
 
 setTimeout(fade_out, 30000);
